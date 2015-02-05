b777e303fe1eb9af9d9a70936b35a8ff1ea8287d
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2015-02-05 13:57:28 +0000

    Add ep_http:url_encode/1

--- src/ep_http.erl
+++ src/ep_http.erl
@@ -3,6 +3,7 @@
 -include("conversions.hrl").
 
 -export([ url_decode/1
+        , url_encode/1
         , etag/1
         ]).
 
@@ -29,8 +30,35 @@ url_decode([X | T], Acc) ->
 url_decode([], Acc) ->
     lists:reverse(Acc, []).
 
+
+-define(is_alphanum(C), C >= $A, C =< $Z; C >= $a, C =< $z; C >= $0, C =< $9).
+
+-type encodable() :: binary() | atom() | [byte()] | integer().
+-spec url_encode(encodable()) -> string().
+url_encode(Str) when is_binary(Str) ->
+    url_encode(binary_to_list(Str));
+url_encode(Str) when is_list(Str) ->
+    url_encode(lists:reverse(Str, []), []);
+url_encode(Str) when is_integer(Str) ->
+    url_encode(integer_to_list(Str));
+url_encode(Str) when is_atom(Str) ->
+    url_encode(atom_to_list(Str)).
+
+-spec url_encode(list(), list()) -> list().
+url_encode([X | T], Acc) when ?is_alphanum(X); X =:= $-; X =:= $_; X =:= $. ->
+    url_encode(T, [X | Acc]);
+url_encode([32 | T], Acc) ->
+    url_encode(T, [$+ | Acc]);
+url_encode([X | T], Acc) ->
+    NewAcc = [$%, hexchr_encode(X bsr 4), hexchr_encode(X band 16#0f) | Acc],
+    url_encode(T, NewAcc);
+url_encode([], Acc) ->
+    Acc.
+
+
 etag(Term) ->
     bin_to_hex(crypto:sha(term_to_binary(Term))).
+
 
 %% ==================================================================
 %% Internal functions
@@ -46,6 +74,14 @@ hexchr_decode(C) when C >= $0 andalso C =< $9 ->
     C - $0;
 hexchr_decode(_) ->
     throw({ep_http, bad_input}).
+
+-compile({inline, [{hexchr_encode, 1}]}).
+
+hexchr_encode(N) when N >= 10 andalso N < 16 ->
+    N + $A - 10;
+hexchr_encode(N) when N >= 0 andalso N < 10 ->
+    N + $0.
+
 
 digit_to_hex(D) when (D >= 0) and (D < 10) ->
     D + 48;
