72d8b723d6d17aa4f068f8a087af89a2a995f487
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2015-02-05 13:59:59 +0000

    Add more lists functions
    * find_first/3
    * shuffle/1
    * pick/1
    * sample/2

--- src/ep_lists.erl
+++ src/ep_lists.erl
@@ -5,6 +5,10 @@
 %% ==================================================================
 
 -export([ find_first/2
+        , find_first/3
+        , shuffle/1
+        , pick/1
+        , sample/2
         ]).
 
 
@@ -18,3 +22,35 @@ find_first(Pred, List) ->
         []    -> false;
         [E|_] -> {value, E}
     end.
+
+find_first(Pred, List, Default) ->
+    case find_first(Pred, List) of
+        {value, E} -> E;
+        false      -> Default
+    end.
+
+
+%% shuffle(List1) -> List2
+%% Takes a list and randomly shuffles it. Relies on random:uniform
+%% inspired by: http://www.trapexit.org/RandomShuffle
+shuffle(List) ->
+    D = [ {random:uniform(), E} || E <- List ],
+    {_, ShuffledList} = lists:unzip(lists:keysort(1, D)),
+    ShuffledList.
+
+
+pick([]) ->
+    undefined;
+pick(List) ->
+    hd(sample(1, List)).
+
+
+%% return a random sample of N elements from a given List
+sample(1, [E]) ->
+    [E];
+sample(1, List) ->
+    lists:sublist(List, random:uniform(length(List)), 1);
+sample(N, List) when N >= length(List) ->
+    List;
+sample(N, List) ->
+    lists:sublist(shuffle(List), N).
